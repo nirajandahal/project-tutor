ffae2a963b3f6d4962a6ae3ea4dd0c039fa64f90
Abhishek Adhikari <abhishekadhikari@Abhisheks-MacBook-Pro.local>
2023-12-26 16:51:18 +0545

    project setup on laravel

---
+++ project-assign/resources/views/home/common/header.blade.php
@@ -0,0 +1,40 @@
+
+<header class="sticky-top border bottom">
+  <div class="header-top primary-color-background">
+    <div class="container py-1">
+      <div class="d-flex justify-content-between">
+        <div class="social-icons">
+          <a href=""><i class="bi bi-facebook"></i></a>
+          <a href=""><i class="bi bi-whatsapp"></i></a>
+          <a href=""><i class="bi bi-instagram"></i></a>
+        </div>
+        <div class="contact-information">
+          +977 9862155336
+        </div>
+        <div class="d-none d-sm-block">
+          <a href="" class="header-link" data-bs-toggle="modal" data-bs-target="#loginModal">Sign In</a>
+        </div>
+      </div>
+    </div>
+  </div>
+    <div class="py-3 bg-white">
+        <div class="container d-flex justify-content-between align-items-center">
+            
+            <div class="fw-bolder display-6"> <a href="index.html" class="pt-logo"><img src="{{asset('home/img/project-tutor-logo.png')}}"></a></div>
+
+            <div class="header-links d-none d-md-block">
+                <ul class="navbar list-unstyled d-flex m-0 p-0">
+                  <li class="nav-item"><a href="index.html" class="nav-link">Home</a></li>
+                  
+                  <li class="nav-item"><a href="index.html#contact-section" class="nav-link pt-cta-btn">Click here to get started!</a></li>
+                </ul>
+            </div>
+
+            <div class="mobile-nav d-md-none">
+              <button class="btn pt-cta-btn" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">More</button>
+            </div>
+
+            
+        </div>    
+    </div>
+</header> 
